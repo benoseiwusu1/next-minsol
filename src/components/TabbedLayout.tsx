119d3bb7a692379bbff9ad37bc87775235e11595
ranselorm <ranselorm@outlook.com>
2025-01-31 15:45:07 +0000

    Modify

--- src/components/TabbedLayout.tsx
+++ src/components/TabbedLayout.tsx
@@ -25,17 +25,34 @@ const committees: Committees = {
       details:
         "Mr Abuah is a Mining and Mineral Engineer by profession with over 30yrs experience in the mineral industry.  Theo holds a MSc (mining), Dip (Mineral Technology) and Certificate in Project Management. He has worked with Goldfields Gh Ltd, AngloGold Ashanti Bibiani mine; currently Mensin Gold (a subsidiary of Asante gold). A member of the Minsol team since 2015, Theo has assisted and led multinational companies as their representative in Ghana and West Africa. Some notable examples include: Polycorp (Canada), Gekko System (Australia) and West African Forging Ltd. He is currently assisting with business development post-establishment of West Africa Conveyor Ltd (WACO); the only indigenous conveyor and Underground Support System manufacturer.",
     },
-    { name: "Carlos F. Cáceres Contreras", position: "Director" },
-    { name: "Alfonso A. Swett Opazo", position: "Director" },
-    { name: "José Ignacio Figueroa Elgueta", position: "Director" },
-    { name: "Alfonso Swett Saavedra", position: "Director" },
-    { name: "Fernando Franke García", position: "Director" },
-    { name: "Rolando Medeiros Soux", position: "Board Advisor" },
+    {
+      name: "Isaac Adusei",
+      position: "Head of Finance",
+      details:
+        "Mr Adusei is a Finance Professional with over 30 years of post-qualification experience. This experience has spanned many industries, including mining, fast-moving consumer goods, textiles, and breweries. In all these places, he functioned as Head of Accounts with responsibilities covering reporting, budgeting, and treasury.",
+    },
   ],
-  "Executive Committee": [
-    { name: "Carlos F. Cáceres Contreras", position: "President" },
-    { name: "Cristóbal Eyzaguirre Baeza", position: "Vicepresident" },
-    { name: "Alfonso Swett Saavedra", position: "Director" },
+  "Administrative & Technical": [
+    {
+      name: "Ananth Achary",
+      position: "Foundry Plant Manager",
+      details:
+        "Mr Achary has over 17 years of experience in marketing, planning, production, quality and, sales management within the foundry industry. During this period, he has been an implementer of the six sigma and 5S methodologies which are used to improve business and reduce the wastage in production process. Mr Achary’s experience has seen in work in various foundries in Ghana and India. He will serve as the first plant manager of the Minsol foundry.",
+    },
+    {
+      name: "Augustine Nuamah",
+      position: "Finance & Administration Executive",
+    },
+    { name: "Alex Owusu", position: "Operations" },
+    { name: "Benedicta Cobbah", position: "HSE Supervisor" },
+    { name: "David Peasah", position: "Operations" },
+    { name: "Edward Sowah", position: "Human Resources Manager" },
+    { name: "Emmanuel Ocran", position: "Group Mechanical Engineer" },
+    { name: "Emmanuel K. Arthur", position: "Technical Sales Engineer" },
+    {
+      name: "Yaw Boateng",
+      position: "Operations & Finance Executive Logistics",
+    },
   ],
   Administration: [
     { name: "Carlos F. Cáceres Contreras", position: "President" },
@@ -49,7 +66,7 @@ const committees: Committees = {
 };
 
 const TabbedLayout: React.FC = () => {
-  const [selectedTab, setSelectedTab] = useState<string>("Board of Directors");
+  const [selectedTab, setSelectedTab] = useState<string>("Senior Management");
 
   return (
     <section className="py-8 h-[70vh] mb-40 md:mb-0">
