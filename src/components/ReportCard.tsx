071401cba8860a223d7df88fc5a0ddc0edd1e987
ranselorm <ranselorm@outlook.com>
2025-02-15 21:31:28 +0000

    Minor update

--- src/components/ReportCard.tsx
+++ src/components/ReportCard.tsx
@@ -8,11 +8,9 @@ interface ReportCardProps {
 const ReportCard: React.FC<ReportCardProps> = ({ title, link }) => {
   return (
     <div className="w-full md:w-[200px] bg-white rounded-lg shadow-lg flex flex-col justify-between p-4 h-[180px]">
-      {/* Title */}
       <div className="flex-grow flex items-center">
         <p className="font-bold text-left">{title}</p>
       </div>
-      {/* View PDF Link */}
       <div className="mt-auto">
         <Link href={link} className="text-orange-400 font-bold hover:underline">
           VIEW PDF
